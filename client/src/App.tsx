00e211bca678f958941cfeb0daf3f4faba952307
Alejandrotb1 <aksieteg@gmail.com>
2025-04-11 20:38:32 -0400

    client corregido

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,13 @@
-import './App.css';
-import Layout from './components/layout/Layout';
+import "./App.css";
+import Layout from "./components/layout/Layout";
 
 function App() {
-
   return (
-    <Layout title='Inicio'>
-
-     <h2>hola</h2>
+    <Layout title="Inicio">
+      <h2>hola</h2>
     </Layout>
   );
 }
 
 export default App;
+
