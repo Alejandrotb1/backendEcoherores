e3ac61a4013f7d65fdc81132baae386c6dd01a02
AdrjGo <gcadrianj@gmail.com>
2025-04-15 23:05:48 -0400

    fix/ responsive desing

--- client/src/pages/EcoHeroes.tsx
+++ client/src/pages/EcoHeroes.tsx
@@ -3,17 +3,20 @@ import { temporalRecolectores } from "../types/temporal-recolectores";
 
 export default function EcoHeroes() {
   return (
-    <div className="flex flex-wrap justify-center sm gap-4">
-      {temporalRecolectores.map((recolector) => (
-        <RecolectorCard
-          rate={recolector.rate}
-          name={recolector.name}
-          ci={recolector.ci}
-          zoneRecolector={recolector.zoneRecolector}
-          photo={recolector.photo}
-          credentialNumber={recolector.credentialNumber}
-        />
-      ))}
-    </div>
+    <section className=" w-[90%] mx-auto my-5">
+      <h2 className=" text-4xl font-bold text-black my-5">Nuestros Eco Heroes</h2>
+      <div className="flex flex-wrap justify-center gap-4">
+        {temporalRecolectores.map((recolector) => (
+          <RecolectorCard
+            rate={recolector.rate}
+            name={recolector.name}
+            ci={recolector.ci}
+            zoneRecolector={recolector.zoneRecolector}
+            photo={recolector.photo}
+            credentialNumber={recolector.credentialNumber}
+          />
+        ))}
+      </div>
+    </section>
   );
 }
